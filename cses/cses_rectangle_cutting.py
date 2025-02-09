f7902fe31d51c04dd7e8bf928edcb46ed9621bd8
KrAbH <kumar.abhishek@alumni.iiit.ac.in>
2025-02-09 13:43:49 +0530

    added tabulation code for problem rectangle cutting

--- cses/cses_rectangle_cutting.py
+++ cses/cses_rectangle_cutting.py
@@ -6,6 +6,7 @@ class Solver():
         self.n = n
         self.m = m
         self.mem = [[[[-1 for _ in range(self.m + 1)] for _ in range(self.m + 1)]for _ in range(self.n + 1)] for _ in range(self.n + 1)]
+        self.dp = [[-1 for _ in range(self.m + 1)] for _ in range(self.n + 1)]
 
     def min_rect_cut_mem(self, x_s, x_e, y_s, y_e):
         if x_e - x_s == y_e - y_s:
@@ -23,8 +24,38 @@ class Solver():
         # mn += 1
         self.mem[x_s][x_e][y_s][y_e] = mn
         return self.mem[x_s][x_e][y_s][y_e]
+    def min_rect_cut_mem_v2(self, x, y):
+        # print(x, y)
+        if x == y:
+            return 0
+        if self.dp[x][y] != -1:
+            return self.dp[x][y]
+        mn = float("inf")
+        for i in range(1, x):
+            mn = min(mn, self.min_rect_cut_mem_v2(i, y) + self.min_rect_cut_mem_v2(x - i, y) + 1)
+        
+        for i in range(1, y):
+            mn = min(mn, self.min_rect_cut_mem_v2(x, i) + self.min_rect_cut_mem_v2(x, y - i) + 1)
+
+        self.dp[x][y] = mn
+        return mn
+    
+    def min_rect_cut_tab(self):
+        tab = [[float("inf") for _ in range(self.m + 1)] for _ in range(self.n + 1)]
+        for x in range(1, self.n +1):
+            for y in range(1, self.m + 1):
+                if x == y:
+                    tab[x][y] = 0
+                else:
+                    for i in range(1, x):
+                        tab[x][y] = min(tab[x][y], 1 + tab[x-i][y] + tab[i][y])
+                    for i in range(1, y):
+                        tab[x][y] = min(tab[x][y], 1+ tab[x][y-i] + tab[x][i])
+        return tab[self.n][self.m]
 if __name__ == "__main__":
     a, b = list(map(int, input().split()))
     solver = Solver(a, b)
-    print(solver.min_rect_cut_mem(0, a, 0, b))
+    # print(solver.min_rect_cut_mem(0, a, 0, b))
+    # print(solver.min_rect_cut_mem_v2(a, b))
+    print(solver.min_rect_cut_tab())
     
